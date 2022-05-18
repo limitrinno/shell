9ac26b9130c7d6a3e88a60fca9806c5b7784f89b
limitrinno <limitrinno@gmail.com>
2022-05-18 11:59:08 +0800

    New UI

--- main.sh
+++ main.sh
@@ -35,18 +35,43 @@ get_curlwget=`ls /usr/bin | grep -E "^curl|^wget" | wc -l`
 
 if [ $get_chos1 == 1 ]; then
     if [ $get_curlwget == '2' ]; then
-        echo -e "${greenbg} Centos 系统环境正常！ ${plain}"
+        echo -e "${greenbg} Centos 系统环境正常！即将进入脚本 ${plain}"
     else
         sudo yum -y install wget curl && clear && echo -e "${greenbg} Centos的Curl和Wget更新完成!!! ${plain}"
     fi
 elif [ $get_chos2 == 1 ]; then
     if [ $get_curlwget == '2' ]; then
-        echo -e "${greenbg} Ubuntu 系统环境正常！ ${plain}"
+        echo -e "${greenbg} Ubuntu 系统环境正常！即将进入脚本 ${plain}"
     else
-        sudo apt-get -y install wget curl && clear
+        sudo apt-get -y install wget curl && clear && echo -e "${greenbg} Ubuntu的Curl和Wget更新完成!!! ${plain}"
     fi
 else
     clear && echo -e "${redbg} 目前脚本大部分只支持Centos7.x和少量支持Ubuntu20.04,其他系统不兼容！！！ ${plain}" && exit
 fi
 
 # ===== 系统依赖检测 =====
+
+
+# ===== 脚本主界面 =====
+menu(){
+echo -e "
+${greenbg}==================== Limit Main V22.05.18 ====================${plain}
+
+${green}1.${plain} 检查服务器系统配置
+${green}2.${plain} Centos7 简单安装Docker
+${green}3.${plain} 临时系统代理(当国内的网络无法到达时使用)
+${green}5.${plain} 路由测试脚本-IPIPnet
+
+${green}0.${plain} 退出脚本输入0
+
+${greenbg}==================== Limit Main V22.05.18 ====================${plain}
+"
+}
+menu
+read -p "请输入选择 [1-10]:" num
+case $num in
+0)  exit;;
+1)  get_sysinfo;;
+*)  echo -e "${red} 选择不存在，重新进入脚本  ${plain}" && bash <(curl -sL http://43.132.193.125:5550/https://github.com/limitrinno/shell/blob/master/main.sh);;
+esac
+# ===== 脚本主界面 =====
